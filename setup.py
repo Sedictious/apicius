6e07f23e9f533620277c38928b003bcb67dab30f
Eleftheria Chatziargyriou <ele.hatzy@gmail.com>
2019-11-04 18:37:56 +0000

    Add setup and requirements files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apicius',
+    version='1.0',
+    packages=['data'],
+    url='',
+    license='MIT License',
+    author='Eleftheria Chatziargyriou',
+    author_email='ele.hatzy@gmail.com',
+    description='Code for the CLTK workshop'
+)
